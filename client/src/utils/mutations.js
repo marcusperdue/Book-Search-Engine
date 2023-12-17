7f31733aa7a30ccee993325f194648723c6a9845
Marcus Perdue <marcusperduee@gmail.com>
2023-12-16 20:05:59 -0800

    defined mutations for save book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -22,5 +22,58 @@ export const LOGIN_USER = gql`
   }
 `;
 
- 
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $authors: [String]!
+    $description: String!
+    $title: String!
+    $bookId: String!
+    $image: String
+    $link: String
+  ) {
+    saveBook(
+      authors: $authors
+      description: $description
+      title: $title
+      bookId: $bookId
+      image: $image
+      link: $link
+    ) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
  

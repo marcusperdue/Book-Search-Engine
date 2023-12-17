15640b09a305108fc10c1a3709dd64e5bc4799bf
Marcus Perdue <marcusperduee@gmail.com>
2023-12-16 19:26:44 -0800

    apply apollo to express and console log graphql server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+
 const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,6 +26,10 @@ const server = new ApolloServer({
   context: authMiddleware,  
 });
 
+server.applyMiddleware({ app });
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => 
+  console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`🚀 GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });

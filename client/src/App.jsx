d9f9e3d4998517bff8a1e3860c383b2717064451
Marcus Perdue <marcusperduee@gmail.com>
2023-12-16 20:13:52 -0800

    create apollo instance for app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: '/graphql',  
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
+     <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
+    </ApolloProvider>
     </>
   );
 }

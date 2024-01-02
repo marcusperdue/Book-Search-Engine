5ab3d8c92b2f1787aa61ddbf9b3d5ffba38a0cdd
Marcus Perdue <marcusperduee@gmail.com>
2024-01-02 15:55:45 -0800

    changed build to dist

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ app.use(express.json());
 
 // Serve static files in production mode
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
 

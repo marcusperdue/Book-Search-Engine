ae3312b4fa088bbf8c2178f765c03156f0e53b56
Marcus Perdue <marcusperduee@gmail.com>
2024-01-03 22:25:57 -0800

    added mongo uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// Connect to MongoDB using the environment variable
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
